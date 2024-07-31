264a07597ba97903d1baac99725470a0fb0891cb
ctismehmetbulut <mehmetbulutengineer@gmail.com>
2024-08-01 02:37:42 +0300

    type support for packages

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,8 @@
-const express = require("express");
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+
 const app = express();
-const cors = require("cors");
-const bodyParser = require("body-parser");
 
 app.use(cors({
   exposedHeaders: ["X-Error-Code"],
@@ -10,7 +11,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Middleware to ignore /favicon.ico requests
-app.use((req, res, next) => {
+app.use((req: any, res: any, next: any) => {
   if (req.originalUrl === "/favicon.ico") {
     res.status(204).end();
   } else {
@@ -18,8 +19,6 @@ app.use((req, res, next) => {
   }
 });
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req: any, res: any) => res.send("Express on Vercel"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
-
-module.exports = app;
