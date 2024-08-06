dd801bf0405671c10969dc283d86234974965920
Mehmet Bulut <78090375+ctismehmetbulut@users.noreply.github.com>
2024-08-06 23:47:15 +0300

    Update build.js for dev and prod differentiation

--- build.js
+++ build.js
@@ -1,5 +1,9 @@
 // build.js
 const { build } = require('esbuild');
+const { execSync } = require('child_process');
+const fs = require('fs');
+
+execSync('rimraf dist/*');
 
 build({
   entryPoints: ['src/app.ts'],
@@ -10,4 +14,11 @@ build({
   external: ['express'], // add any other external dependencies here
   minify: true, // Add this for production builds
   sourcemap: true, // Add this for easier debugging
+}).then(() => {
+  // Optionally clean up all other files in the dist directory except app.js and its sourcemap
+  fs.readdirSync('dist').forEach(file => {
+    if (!file.startsWith('app.js')) {
+      fs.unlinkSync(`dist/${file}`);
+    }
+  });
 }).catch(() => process.exit(1));
