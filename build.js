d8881f778f9073e30c8a376863bf670a434769ff
ctismehmetbulut <mehmetbulutengineer@gmail.com>
2024-08-07 02:18:14 +0300

    removal of the build folder when building for production

--- build.js
+++ build.js
@@ -1,9 +1,16 @@
 // build.js
+const fs = require('fs-extra');
+const path = require('path');
 const { build } = require('esbuild');
-const { execSync } = require('child_process');
 const fs = require('fs');
 
-execSync('rimraf dist/*');
+// Resolve the path to the dist directory
+const distPath = path.resolve(__dirname, 'dist');
+
+// Remove the dist directory
+fs.removeSync(distPath);
+
+console.log('Build directory cleaned.');
 
 build({
   entryPoints: ['src/app.ts'],
@@ -14,11 +21,4 @@ build({
   external: ['express'], // add any other external dependencies here
   minify: true, // Add this for production builds
   sourcemap: true, // Add this for easier debugging
-}).then(() => {
-  // Optionally clean up all other files in the dist directory except app.js and its sourcemap
-  fs.readdirSync('dist').forEach(file => {
-    if (!file.startsWith('app.js')) {
-      fs.unlinkSync(`dist/${file}`);
-    }
-  });
 }).catch(() => process.exit(1));
